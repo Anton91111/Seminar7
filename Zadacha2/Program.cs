32dc672a8bf3c5f060b51eba29a6a663e3065ebf
Anton <anton.esev@mail.ru>
2023-06-11 19:03:23 +0300

    sem7

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента
+ в двумерном массиве, и возвращает значение этого элемента или же указание,
+  что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+
+using System;
+using static System.Console;
+int[,] array = new int[,]
+        {
+            {1, 4, 7, 2},
+            {5, 9, 2, 3},
+            {8, 4, 2, 4}
+        };
+
+Write("Введите номер строки: ");
+int row = int.Parse(ReadLine());
+
+Write("Введите номер столбца: ");
+int column = int.Parse(ReadLine());
+
+if (row >= array.GetLength(0) || column >= array.GetLength(1))
+{
+    WriteLine("Такого элемента нет");
+}
+else
+{
+    int element = array[row, column];
+    WriteLine($"Значение элемента: {element}");
+}
---
+++ Zadacha3/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+using System;
+using static System.Console;
+int[,] numbers = new int[,]
+       {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+       };
+
+
+int columns = numbers.GetLength(0);
+
+for (int i = 0; i < columns; i++)
+{
+    double sum = 0;
+
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum += numbers[j, i];
+    }
+
+    double average = sum / numbers.GetLength(0);
+
+    WriteLine("Cреднее значение по столбцу {0} равно {1}", i + 1, average);
+} 
